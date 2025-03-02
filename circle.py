f9611f9b11b9d430fba20de1b1281c6ce68f112b
Hash-Brown15 <JackHeppell1@gmail.com>
2025-03-02 08:56:44 +0000

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+screen.fill(("white"))
+blue = "sky blue"
+pygame.display.update()
+
+class Circle:
+    def __init__(self,color,pos,radius,width):
+        self.color = color
+        self.pos = pos
+        self.radius = radius
+        self.width = width
+        self.screen = screen
+
+    def draw(self):
+        self.draw_circle = pygame.draw.circle(
+            self.screen,
+            self.color,
+            self.pos,
+            self.radius,
+            self.width
+        )
+    def grow(self,r):
+        self.radius += r
+        self.draw_circle = pygame.draw.circle(
+        self.screen,
+        self.color,
+        self.pos,
+        self.radius,
+        self.width
+        )
+circle = Circle(blue,(300,300),25,0)
+circle.draw()
